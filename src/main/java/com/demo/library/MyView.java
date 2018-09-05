3e4ee293af185ffe28cfd90edc4917e53b61c14f
yejihuang209592 <yejihuang209592@sogou-inc.com>
2018-09-05 10:32:18 +0800

    第一个android依赖库初始化

---
+++ src/main/java/com/demo/library/MyView.java
@@ -0,0 +1,26 @@
+package com.demo.library;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
